f35f1831afa27362829212cf5af79f3348928f88
ashish <ashishvj123@gmail.com>
2023-05-31 23:00:30 +0530

    login page done

---
+++ src/main/java/freshpay/Loginpage.java
@@ -0,0 +1,13 @@
+package freshpay;
+
+public class Loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi this is new login page");
+		
+
+	}
+
+}
